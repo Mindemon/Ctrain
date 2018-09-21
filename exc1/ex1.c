1dcff6064ee3df17157a5d7fc7b2851d22e93e86
Omer <omer@localhost.localdomain>
2018-09-21 21:46:14 +0300

    simple C programs

---
+++ exc1/ex1.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+/* This is a comment */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+	
+	
+	//This is also a comment 
+	printf("You are %d miles away. \n" , distance); 
+	printf("I hope i wont crash this code...\n");
+	printf("when im writing these print functions\n");
+	printf("it took me almost half a day just to set up\n");
+	printf("and start program with C language\n");
+	
+	
+	return 0;
+}
+
